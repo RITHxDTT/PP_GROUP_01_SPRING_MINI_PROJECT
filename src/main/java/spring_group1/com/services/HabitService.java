e2137903e70aa655272915ea60baa2f160b1ad5e
Rosvanda-window <vandaros97@gmail.com>
2026-04-03 23:23:27 +0700

    habit feature

---
+++ src/main/java/spring_group1/com/services/HabitService.java
@@ -0,0 +1,12 @@
+package spring_group1.com.services;
+
+import org.springframework.http.ResponseEntity;
+import spring_group1.com.model.Habit;
+
+import java.util.List;
+
+public interface HabitService {
+
+
+    ResponseEntity<List<Habit>> getAllHabit(Integer page, Integer size);
+}
---
+++ src/main/java/spring_group1/com/services/serviceImpl/HabitServiceImpl.java
@@ -0,0 +1,22 @@
+package spring_group1.com.services.serviceImpl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import spring_group1.com.model.Habit;
+import spring_group1.com.repository.HabitRepository;
+import spring_group1.com.services.HabitService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class HabitServiceImpl implements HabitService {
+
+    private final HabitRepository habitRepository;
+
+    @Override
+    public ResponseEntity<List<Habit>> getAllHabit(Integer page, Integer size) {
+        return habitRepository.findAllHabit(page, size);
+    }
+}
