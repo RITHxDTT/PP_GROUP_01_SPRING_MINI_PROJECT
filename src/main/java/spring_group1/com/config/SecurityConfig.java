c8e2404f7cc6c509409d56f93094f296347eb681
Yung Bunnarith <bunreth552@gmail.com>
2026-04-04 18:22:22 +0700

    bugFixed

--- src/main/java/spring_group1/com/config/SecurityConfig.java
+++ src/main/java/spring_group1/com/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests( auth -> auth
                         .requestMatchers(
                                 "/api/v1/habits/**",
-                                "/api/v1/auth/**",
+                                "/api/v1/auths/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
